46e3b8be2001d26d855df35f75d7b36fcbe53d1d
Antonio Luis Tolentino <63943575+august287@users.noreply.github.com>
2025-08-28 18:34:07 +0800

    - fixed headers again

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "81c9916bcc32faa0"; }
+/******/ 		__webpack_require__.h = function() { return "fb359da62b9782b3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
